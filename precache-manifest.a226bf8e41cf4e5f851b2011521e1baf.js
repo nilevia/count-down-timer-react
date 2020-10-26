44ba8d7da15289b23aba6182f364ab07d971e655
nilevia <otniel@alterra.id>
2020-10-26 10:14:59 +0700

    Updates

---
+++ precache-manifest.a226bf8e41cf4e5f851b2011521e1baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2376838a6428b23c5d5385fdeb82391b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85289d70634bf1076582",
+    "url": "./static/css/main.0659d755.chunk.css"
+  },
+  {
+    "revision": "d46d2bace8de330cc45d",
+    "url": "./static/js/2.847b62af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.847b62af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85289d70634bf1076582",
+    "url": "./static/js/main.5568eb85.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.5568eb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2672c286db899153cf4",
+    "url": "./static/js/runtime-main.4ea5aa97.js"
+  }
+]);
